f0030c9f6f815349fcf2717023a9a4e33220c391
manasavijay09 <manasa.vijay09@gmail.com>
2024-01-24 21:59:46 -0600

    added server.js & package.json files

---
+++ server.js
@@ -0,0 +1,31 @@
+// //import express
+// const express = require('express');
+
+// //path
+// const path = require('path');
+
+// //app variable
+// const app=express();
+
+// //port
+// const PORT = process.env.PORT || 3001;
+
+
+// app.use(express.urlencoded({extended:true}))//req.body (undefined)
+// app.use(express.json())
+// app.use(express.static('public'));
+
+// //import apiroutes
+// require("./routes/apiRoute")(app);
+
+// //import html.js from routes folder
+// require('./routes/html')(app);
+
+
+
+
+// app.listen(PORT, () =>
+//   console.log(`App listening at http://localhost:${PORT}`)
+// );
+
+
